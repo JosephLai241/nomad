3de25d35d32f081860818b96a7a315e6a7987eac
Joseph Lai <jlai24142@gmail.com>
2021-12-27 20:55:07 -0500

    Adding directory flag

---
+++ src/cli.rs
@@ -0,0 +1,20 @@
+//! Defining command-line interface flags.
+
+use structopt::StructOpt;
+
+/// This struct contains all flags that are used in this program.
+#[derive(Debug, PartialEq, StructOpt)]
+#[structopt(name = "oak", about = "The `tree` command, but better.")]
+pub struct Args {
+    #[structopt(
+        short = "d",
+        long = "directory",
+        help = "Display a tree for this directory"
+    )]
+    pub directory: Option<String>,
+}
+
+/// Return the `Args` struct.
+pub fn get_args() -> Args {
+    Args::from_args()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+//! `oak` - The `tree` command, but better.
+
+mod cli;
+
+use std::{env, io::Result};
+
+fn main() -> Result<()> {
+    let args = cli::get_args();
+
+    if let Some(directory) = args.directory {
+        println!("The directory passed into the `d` flag is: {}", directory);
+        Ok(())
+    } else {
+        // Get the current directory as the target for oak if no target was entered.
+        let directory = env::current_dir()?;
+        println!("The current directory is: {}", directory.display());
+        Ok(())
+    }
 }
