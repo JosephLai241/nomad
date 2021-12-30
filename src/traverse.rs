e6e23a525786113179368c8044294277b0ed704a
Joseph Lai <jlai24142@gmail.com>
2021-12-30 17:23:02 -0500

    Finished traversal algorithms and associated functions

--- src/traverse.rs
+++ src/traverse.rs
@@ -1,52 +1,121 @@
 //! Traverse the target directory while respecting rules set in ignore-type files.
 
-use std::{env, io::Error};
-
 use crate::cli::Args;
 
-use ignore::{DirEntry, Walk, WalkBuilder};
+use ansi_term::*;
+use ignore::{self, DirEntry, Walk, WalkBuilder};
+use ptree::{print_tree, TreeBuilder};
 
-pub fn build_walker(args: Args) -> Result<Walk, Error> {
-    // Set the target directory based on whether a directory was passed into `oak`.
-    let target_directory;
-    if let Some(directory) = args.directory {
-        target_directory = directory;
-    } else {
-        // Get the current directory as the target for oak if no target was entered.
-        // Panic if Rust can't get the current directory.
-        let directory = env::current_dir()?;
-        target_directory = directory.to_str().unwrap().into();
-    }
+use std::{collections::HashMap, ffi::OsStr, io::Error, path::PathBuf, time::Instant};
 
-    println!("\nTHE TARGET DIRECTORY IS: {}\n", target_directory);
-
-    Ok(WalkBuilder::new(target_directory)
+/// Build a `Walk` object based on the client's CLI parameters.
+pub fn build_walker(args: &Args, target_directory: &str) -> Walk {
+    WalkBuilder::new(target_directory)
         .git_exclude(!args.disrespect)
         .git_global(!args.disrespect)
         .git_ignore(!args.disrespect)
         .ignore(!args.disrespect)
         .hidden(!args.hidden)
         .parents(!args.disrespect)
-        .build())
+        .sort_by_file_path(|a, b| a.cmp(b))
+        .build()
 }
 
-fn display_directory(entry: DirEntry) {
-    println!("DIRECTORY IS: {:?}", entry.path().display());
+/// Format how the item will be displayed in the tree.
+fn format_item(extension_icon_map: &HashMap<&str, &str>, is_dir: bool, item: &DirEntry) -> String {
+    let icon = if is_dir {
+        &"\u{f115}"
+    } else {
+        extension_icon_map
+            .get(
+                item.path()
+                    .extension()
+                    .unwrap_or(OsStr::new("markdown"))
+                    .to_str()
+                    .unwrap(),
+            )
+            .unwrap_or(&&"\u{f016}") // 
+    };
+
+    let formatted_item = item
+        .path()
+        .file_name()
+        .unwrap_or(OsStr::new("?"))
+        .to_str()
+        .unwrap_or("?");
+
+    if is_dir {
+        return format!(
+            "{} {}",
+            icon,
+            Colour::Blue.bold().paint(
+                item.path()
+                    .file_name()
+                    .unwrap_or(OsStr::new("?"))
+                    .to_str()
+                    .unwrap_or("?")
+            )
+        );
+    }
+
+    format!("{} {}", icon, formatted_item)
 }
 
-fn display_file(entry: DirEntry) {
-    println!("FILE IS: {:?}", entry.file_name().to_str());
-}
+/// Traverse the directory and display files and directories accordingly.
+pub fn walk_directory(
+    args: &Args,
+    extension_icon_map: &HashMap<&str, &str>,
+    target_directory: &str,
+    walker: &mut Walk,
+) -> Result<(), Error> {
+    let mut current_depth: usize = 0;
+    let mut tree = TreeBuilder::new(format!(
+        "{} {}",
+        &"\u{f115}",
+        Colour::Blue.bold().paint(
+            PathBuf::from(target_directory)
+                .canonicalize()
+                .unwrap_or(PathBuf::from("?"))
+                .file_name()
+                .unwrap_or(OsStr::new("?"))
+                .to_str()
+                .unwrap_or("?")
+        )
+    ));
+    walker.next(); // Skip the target directory.
+    println!();
 
-pub fn walk_directory(mut walker: Walk) -> Result<(), Error> {
-    while let Some(Ok(entry)) = walker.next() {
-        if entry.path().is_dir() {
-            display_directory(entry);
-        } else if entry.path().is_file() {
-            display_file(entry);
-        } else {
-            unimplemented!();
+    let mut num_directories = 0;
+    let mut num_files = 0;
+    let start = Instant::now();
+    while let Some(Ok(item)) = walker.next() {
+        if item.depth() < current_depth {
+            for _ in 0..current_depth - item.depth() {
+                tree.end_child();
+            }
         }
+
+        if item.path().is_dir() {
+            tree.begin_child(format_item(extension_icon_map, true, &item));
+            num_directories += 1;
+        } else if item.path().is_file() {
+            tree.add_empty_child(format_item(extension_icon_map, false, &item));
+            num_files += 1;
+        }
+
+        current_depth = item.depth();
+    }
+
+    print_tree(&tree.build())?;
+    println!();
+
+    if args.statistics {
+        println!(
+            "{} directories | {} files | {} ms\n",
+            num_directories,
+            num_files,
+            start.elapsed().as_millis()
+        );
     }
 
     Ok(())
