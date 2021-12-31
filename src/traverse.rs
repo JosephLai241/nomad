c0c2e20a9af10e20c225dd87ef5d05461f35eb28
Joseph Lai <jlai24142@gmail.com>
2021-12-31 02:46:51 -0500

    Integrating quick open features. Some refactoring

--- src/traverse.rs
+++ src/traverse.rs
@@ -1,10 +1,16 @@
 //! Traverse the target directory while respecting rules set in ignore-type files.
 
-use crate::cli::Args;
+use crate::{
+    cli::Args,
+    utils::temp::{
+        create_temp_dir, get_json_file, set_current_dir, should_overwrite, write_to_json,
+    },
+};
 
 use ansi_term::*;
 use ignore::{self, DirEntry, Walk, WalkBuilder};
 use ptree::{print_tree, TreeBuilder};
+use serde_json::json;
 
 use std::{collections::HashMap, ffi::OsStr, io::Error, path::PathBuf, time::Instant};
 
@@ -21,6 +27,16 @@ pub fn build_walker(args: &Args, target_directory: &str) -> Walk {
         .build()
 }
 
+/// Get the absolute path of a directory.
+fn canonicalize_path(target_directory: &str) -> String {
+    PathBuf::from(target_directory)
+        .canonicalize()
+        .unwrap_or(PathBuf::from("?"))
+        .into_os_string()
+        .into_string()
+        .unwrap_or("?".into())
+}
+
 /// Format how the item will be displayed in the tree.
 fn format_item(
     extension_icon_map: &HashMap<&str, &str>,
@@ -29,13 +45,13 @@ fn format_item(
     number: Option<i32>,
 ) -> String {
     let icon = if is_dir {
-        &"\u{f115}"
+        &"\u{f115}" // 
     } else {
         extension_icon_map
             .get(
                 item.path()
                     .extension()
-                    .unwrap_or(OsStr::new("markdown"))
+                    .unwrap_or(OsStr::new("none"))
                     .to_str()
                     .unwrap(),
             )
@@ -99,6 +115,7 @@ pub fn walk_directory(
 
     let mut num_directories = 0;
     let mut num_files = 0;
+    let mut items: Vec<Vec<String>> = Vec::new();
     let start = Instant::now();
     while let Some(Ok(item)) = walker.next() {
         if item.depth() < current_depth {
@@ -111,12 +128,46 @@ pub fn walk_directory(
             tree.begin_child(format_item(extension_icon_map, true, &item, None));
             num_directories += 1;
         } else if item.path().is_file() {
-            let number = if args.numbers { Some(num_files) } else { None };
+            let number;
+            if args.numbers {
+                number = Some(num_files);
+
+                items.push(vec![
+                    num_files.to_string(),
+                    item.path()
+                        .canonicalize()
+                        .unwrap_or(PathBuf::from("?"))
+                        .into_os_string()
+                        .into_string()
+                        .unwrap_or("?".into()),
+                ])
+            } else {
+                number = None;
+            };
             tree.add_empty_child(format_item(extension_icon_map, false, &item, number));
             num_files += 1;
         }
 
         current_depth = item.depth();
+    }
+
+    if args.numbers {
+        create_temp_dir()?;
+
+        if should_overwrite(canonicalize_path(target_directory))? {
+            set_current_dir(canonicalize_path(target_directory))?;
+
+            let mut json = json!({ "items": {} });
+            for item in items {
+                json["items"]
+                    .as_object_mut()
+                    .unwrap()
+                    .insert(item[0].clone(), json!(item[1]));
+            }
+
+            let mut json_file = get_json_file(false)?;
+            write_to_json(&mut json_file, json)?;
+        }
     }
 
     print_tree(&tree.build())?;
