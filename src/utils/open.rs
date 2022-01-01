8727f00698d9e1aa2a2002dfe08e133ea6799aef
Joseph Lai <jlai24142@gmail.com>
2021-12-31 22:29:17 -0500

    Adding helper functions to open a file with nomad

--- src/utils/open.rs
+++ src/utils/open.rs
@@ -8,17 +8,28 @@ use serde_json::{self, from_str};
 use std::{
     env::var,
     io::{Error, ErrorKind, Read},
-    process::Command,
+    process::{Command, ExitStatus},
 };
 
-/// Get the default text editor from the environment. If that environment variable
-/// is not set, search for installed text editors, then open the file with one
-/// of the installed editors.
-fn get_text_editor() -> Result<String, Error> {
-    let editor = var("EDITOR").expect("Could not get the system's default editor!");
-    println!("EDITOR IS {}", editor);
+/// Open the target file with an editor.
+fn spawn_editor(editor: String, file: String) -> Result<ExitStatus, Error> {
+    Command::new(editor).arg(&file).status()
+}
 
-    Ok(editor)
+/// Get the default text editor from the environment. If that environment variable
+/// is not set, try to open the file with Neovim, then Vim, and finally Nano.
+fn get_text_editors() -> Vec<String> {
+    let editors = var("EDITOR").map_or_else(
+        |_| {
+            vec!["nvim", "vim", "nano"]
+                .into_iter()
+                .map(|editor| editor.to_string())
+                .collect()
+        },
+        |editor| vec![editor],
+    );
+
+    editors
 }
 
 /// Search for the target file by parsing the JSON file and retrieving the value
@@ -41,10 +52,38 @@ fn search_for_file(target_index: String) -> Result<Option<String>, Error> {
 /// Open the target file.
 pub fn open_file(target_index: String) -> Result<(), Error> {
     search_for_file(target_index)?.map_or_else(
-        || Err(Error::new(ErrorKind::NotFound, "That file does not exist!")),
-        |filename| {
-            println!("FOUND FILE: {}", filename);
-            Ok(())
+        || {
+            Err(Error::new(
+                ErrorKind::NotFound,
+                "That file does not reside in the current tree!",
+            ))
+        },
+        |file| {
+            let editors = get_text_editors();
+            if editors.len() == 1 {
+                spawn_editor(editors[0].to_string(), file).map_or_else(
+                    |error| Err(error),
+                    |status_code| {
+                        println!("{}", status_code);
+                        Ok(())
+                    },
+                )
+            } else {
+                for editor in editors {
+                    match spawn_editor(editor, file.to_string()) {
+                        Ok(status_code) => {
+                            println!("{}", status_code);
+                            return Ok(());
+                        }
+                        Err(_) => {}
+                    };
+                }
+
+                Err(Error::new(
+                    ErrorKind::NotFound,
+                    "Could not open the file with your $EDITOR, Neovim, Vim, or Nano! Do you have one of these editors installed?",
+                ))
+            }
         },
     )
 }
