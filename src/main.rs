c7b5e478ee3c9eb16d25533c33b53c20d87735b4
Joseph Lai <jlai24142@gmail.com>
2022-01-09 13:01:13 -0500

    main() is now a tokio::main function. Calling get_file() so that the 'open' and 'bat' commands can take a filename or index

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@
 mod cli;
 mod models;
 mod traverse;
+mod ui;
 mod utils;
+
+use tokio;
+use ui::spawn_terminal;
 
 use std::{env, io::Result};
 
-use utils::icons::{get_icons_by_extension, get_icons_by_name};
+use utils::{
+    icons::{get_icons_by_extension, get_icons_by_name},
+    open::get_file,
+};
 
 /// Run `nomad`.
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = cli::get_args();
 
     // Set the target directory based on whether a directory was passed in.
@@ -31,11 +39,16 @@ fn main() -> Result<()> {
     let name_icon_map = get_icons_by_name();
 
     if args.interactive {
-        unimplemented!()
-    } else if let Some(target_file) = args.open {
+        let mut walker = traverse::build_walker(&args, &target_directory)?;
+        let _ = spawn_terminal(&args, &target_directory, &mut walker)
+            .await
+            .map_err(|error| format!("UI ERROR: {}", error));
+    } else if let Some(target) = args.open {
+        let target_file = get_file(target)?;
         utils::open::open_file(target_file)?;
-    } else if let Some(file) = args.bat {
-        utils::bat::run_bat(file)?;
+    } else if let Some(target) = args.bat {
+        let target_file = get_file(target)?;
+        utils::bat::run_bat(target_file)?;
     } else {
         let mut walker = traverse::build_walker(&args, &target_directory)?;
         traverse::walk_directory(&args, &extension_icon_map, &target_directory, &mut walker)?;
