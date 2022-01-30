9fa32057df4e63de08335bf6be77a947c2d2a688
Joseph Lai <jlai24142@gmail.com>
2022-01-30 15:01:26 -0500

    Adding export functionality

---
+++ src/utils/export.rs
@@ -0,0 +1,40 @@
+//! Export a directory's tree to a file instead of saving.
+
+use ansi_term::*;
+use ptree::{item::StringItem, write_tree};
+
+use std::{
+    env,
+    fs::File,
+    io::{Error, ErrorKind},
+};
+
+/// Get the absolute path for the file name.
+fn get_absolute_path(file_name: &str) -> Result<String, Error> {
+    Ok(env::current_dir()?
+        .join(file_name)
+        .into_os_string()
+        .into_string()
+        .expect("Could not get the current directory!")
+        .clone())
+}
+
+pub fn export_tree(file_name: String, tree: StringItem) -> Result<(), Error> {
+    let file_path = get_absolute_path(&file_name)?;
+    let file = File::create(&file_path)?;
+
+    write_tree(&tree, file).map_or(
+        Err(Error::new(
+            ErrorKind::Other,
+            "Unable to export directory tree to {file_path}!",
+        )),
+        |_| {
+            let success_message = Colour::Green
+                .bold()
+                .paint(format!("Tree was exported to {file_path}\n"));
+            println!("{success_message}");
+
+            Ok(())
+        },
+    )
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,7 @@
 //! Utilities used throughout this program.
 
 pub mod bat;
+pub mod export;
 pub mod icons;
 pub mod open;
 pub mod temp;
