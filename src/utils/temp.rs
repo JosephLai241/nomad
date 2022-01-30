5d0080fc516c374db655c95a76ece55844f0d12b
Joseph Lai <jlai24142@gmail.com>
2022-01-30 01:43:28 -0500

    Removing unused functions and static variables

--- src/utils/temp.rs
+++ src/utils/temp.rs
@@ -3,14 +3,12 @@
 use std::{
     env::temp_dir,
     fs::{create_dir_all, File},
-    io::{BufRead, BufReader, Error, Write},
+    io::{Error, Write},
     path::PathBuf,
 };
 
 use serde_json::Value;
 
-/// The name of the file that will store the name of the most recently traversed directory.
-static CURRENT: &str = "current";
 /// Directory name within the system's temporary directory to store `nomad` metadata.
 static DIRECTORY: &str = "nomad";
 /// The JSON file that stores directory `Item`s for quick access.
@@ -28,40 +26,6 @@ pub fn get_temp_dir_path() -> PathBuf {
 pub fn create_temp_dir() -> Result<(), Error> {
     create_dir_all(get_temp_dir_path())?;
     Ok(())
-}
-
-/// Create a file in the temporary directory to store the most recently traversed directory.
-pub fn set_current_dir(directory_name: String) -> Result<(), Error> {
-    let mut current_dir_file = get_temp_dir_path();
-    current_dir_file.push(CURRENT);
-
-    let mut file = File::create(current_dir_file)?;
-    file.write_all(directory_name.as_bytes())?;
-
-    Ok(())
-}
-
-/// Determine if `items.json` needs to be overwritten (a new directory was traversed).
-pub fn should_overwrite(target_directory: String) -> Result<bool, Error> {
-    let mut existing_dir_file = get_temp_dir_path();
-    existing_dir_file.push(CURRENT);
-
-    File::open(existing_dir_file).map_or(
-        {
-            set_current_dir(target_directory.clone())?;
-            Ok(true)
-        },
-        |file| {
-            let mut existing_directory = String::new();
-            BufReader::new(file).read_line(&mut existing_directory)?;
-
-            if existing_directory == target_directory {
-                Ok(false)
-            } else {
-                Ok(true)
-            }
-        },
-    )
 }
 
 /// Return the `items.json` `File` object in write/overwrite or read-only mode.
