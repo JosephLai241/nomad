6e61d7288964a3823e1b23b3fee8d40adb4a86f9
Joseph Lai <jlai24142@gmail.com>
2022-02-04 01:45:32 -0500

    Incorporating Git repository functionality. Moving get current directory code block to paths.rs

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,18 @@ mod traverse;
 mod ui;
 mod utils;
 
+use ansi_term::Colour;
 use tokio;
-use ui::spawn_terminal;
 
-use std::{env, io::Result};
+use std::io::Result;
 
+use cli::{Git, GitOptions};
+use ui::{spawn_terminal, utils::convert_tree};
 use utils::{
+    git::get_repo,
     icons::{get_icons_by_extension, get_icons_by_name},
     open::get_file,
+    paths::get_current_directory,
 };
 
 /// Run `nomad`.
@@ -25,20 +29,30 @@ async fn main() -> Result<()> {
     let target_directory = if let Some(ref directory) = args.directory {
         directory.clone()
     } else {
-        // TODO: MOVE THIS TO UTILS?
-        // Get the current directory as the target if no target was entered.
-        env::current_dir()?
-            .into_os_string()
-            .into_string()
-            .expect("Could not get the current directory!")
-            .clone()
+        get_current_directory().unwrap_or("?".to_string())
     };
 
     let extension_icon_map = get_icons_by_extension();
     let name_icon_map = get_icons_by_name();
 
+    let repo = Some(get_repo(&target_directory)).flatten();
+
     if args.interactive {
+        // TODO: RESERVE FOR NOMAD V0.1.1?
+
         let mut walker = traverse::build_walker(&args, &target_directory)?;
+        let tree_items = traverse::walk_directory(
+            &args,
+            &extension_icon_map,
+            &name_icon_map,
+            repo,
+            &mut walker,
+        )?;
+        if let Some((tree, config)) = tree_items {
+            let tree_parts = convert_tree(config, tree);
+            println!("{:?}", tree_parts);
+        }
+
         let _ = spawn_terminal(&args, &target_directory, &mut walker)
             .await
             .map_err(|error| format!("UI ERROR: {error}"));
@@ -48,10 +62,42 @@ async fn main() -> Result<()> {
     } else if let Some(target) = args.bat {
         let target_file = get_file(target)?;
         utils::bat::run_bat(target_file)?;
+    } else if args.git.is_some() {
+        ////////////////// TODO: REFACTOR?
+        if repo.is_some() {
+            if let Some(sub_command) = args.git {
+                match sub_command {
+                    Git::Git(git_command) => match git_command {
+                        GitOptions::Add { file_number } => {
+                            let target_file = get_file(file_number.to_string())?;
+                            println!("\nTARGET FILE FROM GIT ADD IS: {target_file}\n");
+                            // TODO: ADD METHOD TO DO A `git add <file_number>`
+                        }
+                        GitOptions::Diff { file_number } => {
+                            let target_file = get_file(file_number.to_string())?;
+                            utils::bat::run_bat(target_file)?;
+                        }
+                        _ => {}
+                    },
+                }
+            }
+        } else {
+            println!(
+                "\n{}\n",
+                Colour::Red
+                    .bold()
+                    .paint("Cannot run Git commands in this directory. Not a Git repository!")
+            );
+        }
     } else {
         let mut walker = traverse::build_walker(&args, &target_directory)?;
-        let tree_items =
-            traverse::walk_directory(&args, &extension_icon_map, &name_icon_map, &mut walker)?;
+        let tree_items = traverse::walk_directory(
+            &args,
+            &extension_icon_map,
+            &name_icon_map,
+            repo,
+            &mut walker,
+        )?;
 
         if let (Some(file_name), Some((tree, config))) = (args.export, tree_items) {
             utils::export::export_tree(config, file_name, tree)?;
