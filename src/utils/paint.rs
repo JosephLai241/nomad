9326a078beea8ba6418fcb1bc74532cd1fa96713
Joseph Lai <jlai24142@gmail.com>
2022-02-03 22:03:57 -0500

    Moving painting/color formatting to its own module

---
+++ src/utils/paint.rs
@@ -0,0 +1,36 @@
+//! Apply colors to a directory's contents.
+
+use ansi_term::Colour;
+use ignore::DirEntry;
+
+use std::{fs::read_link, path::PathBuf};
+
+use super::paths::get_filename;
+
+/// Paint a directory.
+pub fn paint_directory(item: &DirEntry) -> String {
+    let directory_name = get_filename(item);
+    Colour::Blue
+        .bold()
+        .paint(format!("{directory_name}"))
+        .to_string()
+}
+
+/// Paint a symlinked directory.
+pub fn paint_symlink_directory(item: &DirEntry) -> String {
+    let filename = get_filename(item);
+
+    let points_to = read_link(item.path()).map_or("?".to_string(), |pathbuf_path| {
+        pathbuf_path
+            .canonicalize()
+            .unwrap_or(PathBuf::from("?"))
+            .into_os_string()
+            .into_string()
+            .map_or("?".to_string(), |path_string| path_string)
+    });
+
+    Colour::Yellow
+        .bold()
+        .paint(format!("{filename} ⇒ {points_to}"))
+        .to_string()
+}
