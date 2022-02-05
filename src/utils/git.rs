29bc6d00c2c0070db7e744729f4bbaa2d9f284a4
Joseph Lai <jlai24142@gmail.com>
2022-02-05 01:29:53 -0500

    Adding two functions get_repo_branch() and extend_marker_map() to support Git functionality. Minor refactoring

--- src/utils/git.rs
+++ src/utils/git.rs
@@ -1,7 +1,7 @@
 //! Providing Git functionality in tree form.
 
 use ansi_term::Colour;
-use git2::{Error, Repository, Status, StatusOptions, StatusShow};
+use git2::{Branch, Error, Repository, Status, StatusOptions, StatusShow};
 
 use std::collections::HashMap;
 
@@ -9,22 +9,58 @@ use super::paths::canonicalize_path;
 
 /// Try to get Git metadata from the target directory.
 pub fn get_repo(target_directory: &str) -> Option<Repository> {
-    Repository::open(target_directory).map_or_else(
-        |_| None,
-        |repo| {
-            if repo.is_bare() {
-                println!("\n{}", Colour::Fixed(172).paint("Git repository is bare!"));
-                None
-            } else {
-                Some(repo)
-            }
-        },
-    )
+    if let Ok(repo) = Repository::open(target_directory) {
+        if repo.is_bare() {
+            println!("\n{}", Colour::Fixed(172).paint("Git repository is bare!"));
+            None
+        } else {
+            Some(repo)
+        }
+    } else {
+        None
+    }
+}
+
+/// Try to get the current Git branch's name.
+pub fn get_repo_branch(repo: &Repository) -> Option<String> {
+    if let Ok(reference) = repo.head() {
+        if let Ok(Some(name)) = Branch::wrap(reference).name() {
+            let branch_name = name.to_string();
+            Some(branch_name)
+        } else {
+            println!(
+                "\n{}\n",
+                Colour::Red
+                    .bold()
+                    .paint("Could not get the current Git branch name!")
+            );
+            None
+        }
+    } else {
+        println!(
+            "\n{}\n",
+            Colour::Red.bold().paint("Could not get repository HEAD!")
+        );
+        None
+    }
+}
+
+/// Try to extend the `HashMap` containing status markers and their corresponding
+/// filenames with new Git repository items.
+pub fn extend_marker_map(git_markers: &mut HashMap<String, String>, target_directory: &str) {
+    if let Some(repo) = get_repo(target_directory) {
+        if let Ok(top_level_map) = get_status_markers(repo, target_directory) {
+            git_markers.extend(top_level_map);
+        }
+    }
 }
 
 /// Get the status markers (colored initials) that correspond with the Git status
 /// of tracked files in the repository.
-pub fn get_status_markers(repo: Repository) -> Result<HashMap<String, String>, Error> {
+pub fn get_status_markers(
+    repo: Repository,
+    target_directory: &str,
+) -> Result<HashMap<String, String>, Error> {
     let mut status_options = StatusOptions::new();
     status_options
         .show(StatusShow::IndexAndWorkdir)
@@ -34,15 +70,15 @@ pub fn get_status_markers(repo: Repository) -> Result<HashMap<String, String>, E
     let mut formatted_items = HashMap::new();
 
     for repo_item in repo.statuses(Some(&mut status_options))?.iter() {
-        let item_name =
-            canonicalize_path(repo_item.path().unwrap_or("?")).unwrap_or("?".to_string());
+        let item_path = format!("{target_directory}/{}", repo_item.path().unwrap_or("?"));
+        let item_name = canonicalize_path(&item_path).unwrap_or("?".to_string());
 
         match repo_item.status() {
             s if s.contains(Status::INDEX_DELETED) => {
                 formatted_items.insert(item_name, Colour::Red.bold().paint("D").to_string());
             }
             s if s.contains(Status::INDEX_MODIFIED) => {
-                formatted_items.insert(item_name, Colour::Yellow.bold().paint("M").to_string());
+                formatted_items.insert(item_name, Colour::Purple.bold().paint("M").to_string());
             }
             s if s.contains(Status::INDEX_NEW) => {
                 formatted_items.insert(item_name, Colour::Green.bold().paint("A").to_string());
@@ -54,7 +90,7 @@ pub fn get_status_markers(repo: Repository) -> Result<HashMap<String, String>, E
                 formatted_items.insert(item_name, Colour::Red.bold().paint("D").to_string());
             }
             s if s.contains(Status::WT_MODIFIED) => {
-                formatted_items.insert(item_name, Colour::Blue.bold().paint("M").to_string());
+                formatted_items.insert(item_name, Colour::Yellow.bold().paint("M").to_string());
             }
             s if s.contains(Status::WT_NEW) => {
                 formatted_items.insert(item_name, Colour::Green.bold().paint("U").to_string());
