918bab3dd51a14d5d824deca342818c416d6f564
Joseph Lai <jlai24142@gmail.com>
2022-02-19 20:58:12 -0500

    Adding two methods - get_repo_diffs() and colorize_origin() for Git diff functionality

--- src/git/diff.rs
+++ src/git/diff.rs
@@ -1,6 +1,27 @@
 //! Exposing functionality for the Git diff command.
 
-use git2::{Index, Repository, Tree};
+use ansi_term::Colour;
+use anyhow::Result;
+use git2::{Diff, Error, Index, ObjectType, Repository, Tree};
+
+/// Get the diff between the old Git tree and the working directory using the Git index.
+pub fn get_repo_diffs<'a>(repo: &'a Repository) -> Result<Diff<'a>, Error> {
+    let previous_head = repo.head()?.peel(ObjectType::Tree)?.id();
+    let old_tree = repo.find_tree(previous_head)?;
+
+    let diff = repo.diff_tree_to_workdir_with_index(Some(&old_tree), None)?;
+
+    Ok(diff)
+}
+
+/// Colorize the origin of the `DiffLine`.
+pub fn colorize_origin(marker: char) -> String {
+    match marker {
+        '+' | '>' => Colour::Green.bold().paint(format!("{marker}")).to_string(),
+        '-' | '<' => Colour::Red.bold().paint(format!("{marker}")).to_string(),
+        _ => Colour::White.bold().paint(format!("{marker}")).to_string(),
+    }
+}
 
 /// Get Git diff statistics by comparing the HEAD and index.
 pub fn get_diff_stats(
