6b9883779b9a159838bade6b49e91783bf9bafc5
Joseph Lai <jlai24142@gmail.com>
2022-02-26 05:15:17 -0500

    Removing unused functions/structs

--- src/git/utils.rs
+++ src/git/utils.rs
@@ -1,18 +1,10 @@
 //! Contains useful utilities that support Git functionality.
 
-use crate::{errors::NomadError, utils::open::get_deserialized_json};
-
-use std::{
-    collections::HashMap,
-    path::{Component, Path},
-};
+use crate::errors::NomadError;
 
 use ansi_term::Colour;
 use anyhow::{anyhow, Result};
 use git2::{Branch, Commit, ObjectType, Repository};
-use itertools::Itertools;
-
-use super::markers::get_status_markers;
 
 /// Try to discover a Git repository at or above the current path.
 fn discover_repo(target_directory: &str) -> Option<Repository> {
@@ -74,73 +66,6 @@ pub fn get_last_commit(repo: &Repository) -> Result<Commit, NomadError> {
             "Could not find the last commit in this Git repository!"
         ))
     })
-}
-
-/// Contains metadata for each modified item in the Git repository.
-#[derive(Debug)]
-pub struct ModifiedItem {
-    /// The filepath broken down into its individual components.
-    pub components: Vec<String>,
-    /// The depth of the file relative to the root of the Git repository.
-    pub depth: i32,
-    /// The Git status marker indicating the change that was made to the file.
-    pub marker: String,
-    /// The filepath.
-    pub path: String,
-}
-
-/// Get the depth of each staged item and transform the HashMap into a Vec of `ModifiedItem`s.
-pub fn add_marker_depths(sliced_markers: HashMap<String, String>) -> Vec<ModifiedItem> {
-    let mut markers = Vec::new();
-
-    for (path, marker) in sliced_markers.iter().sorted() {
-        let item = Path::new(path);
-
-        let mut components = Vec::new();
-        let mut depth = 0;
-        for component in item.components() {
-            match component {
-                Component::Normal(section) => {
-                    components.push(section.to_str().unwrap_or("?").to_string());
-                    depth += 1;
-                }
-                _ => {}
-            }
-        }
-
-        markers.push(ModifiedItem {
-            components,
-            depth,
-            marker: marker.to_owned(),
-            path: path.to_owned(),
-        });
-    }
-
-    markers
-}
-
-/// Strip prefix paths (the absolute path preceding the current target directory)
-/// from existing paths in the Git status marker map.
-pub fn strip_prefixes(
-    current_dir: &str,
-    marker_map: HashMap<String, String>,
-) -> HashMap<String, String> {
-    marker_map
-        .iter()
-        .map(|(key, value)| {
-            let key_path = Path::new(key);
-            let stripped_key = key_path
-                .strip_prefix(current_dir)
-                .expect(&format!(
-                    "Could not strip path prefix for {}!",
-                    key_path.to_str().unwrap_or("?")
-                ))
-                .to_str()
-                .unwrap_or("?");
-
-            (stripped_key.to_owned(), value.to_owned())
-        })
-        .collect()
 }
 
 /// Add color/style to the filename depending on its Git status.
