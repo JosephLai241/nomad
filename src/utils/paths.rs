2eb8d0d0647415861aea7974d803bcc367ede2c4
Joseph Lai <jlai24142@gmail.com>
2022-02-26 05:07:32 -0500

    Adding a function that stylizes a regex-matched path

--- src/utils/paths.rs
+++ src/utils/paths.rs
@@ -1,15 +1,17 @@
 //! Miscellaneous utilities for dealing with file paths.
 
+use crate::errors::NomadError;
+
+use ansi_term::Colour;
 use anyhow::{Context, Result};
 use ignore::DirEntry;
+use regex::Match;
 
 use std::{
     env,
     ffi::OsStr,
     path::{Path, PathBuf},
 };
-
-use crate::errors::NomadError;
 
 /// Get the current directory.
 pub fn get_current_directory() -> Result<String, NomadError> {
@@ -40,6 +42,35 @@ pub fn canonicalize_path(target: &str) -> Result<String, NomadError> {
 pub fn get_filename(item: &Path) -> String {
     item.file_name()
         .unwrap_or(OsStr::new("?"))
+        .to_str()
+        .unwrap_or("?")
+        .to_string()
+}
+
+/// Stylize a matched path based on the target regex pattern.
+pub fn format_regex_match(entry: &DirEntry, matched: Match) -> String {
+    let mut item_name = entry
+        .path()
+        .file_name()
+        .unwrap_or(OsStr::new("?"))
+        .to_str()
+        .unwrap_or("?")
+        .to_string();
+    let matched_section = Colour::Red
+        .bold()
+        .paint(format!(
+            "{}",
+            item_name[matched.start()..matched.end()].to_string()
+        ))
+        .to_string();
+
+    item_name.replace_range(matched.start()..matched.end(), &matched_section);
+
+    entry
+        .path()
+        .parent()
+        .unwrap_or(Path::new("?"))
+        .join(Path::new(&item_name))
         .to_str()
         .unwrap_or("?")
         .to_string()
