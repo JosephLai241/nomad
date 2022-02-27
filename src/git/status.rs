0c2176ecad187dbeb003744c8291a2d73c1d15e1
Joseph Lai <jlai24142@gmail.com>
2022-02-27 03:32:28 -0500

    Enabling pattern matching in the Git status tree. Updating FoundItem initializations to include the new 'matched' field

--- src/git/status.rs
+++ src/git/status.rs
@@ -11,11 +11,13 @@ use crate::{
 };
 
 use ansi_term::Colour;
+use anyhow::private;
 use git2::Repository;
 use itertools::Itertools;
 use ptree::{item::StringItem, PrintConfig};
+use regex::Regex;
 
-use std::collections::HashMap;
+use std::{collections::HashMap, ffi::OsStr, path::Path};
 
 /// Build a tree that only contains items that are tracked in Git.
 pub fn display_status_tree(
@@ -45,14 +47,48 @@ pub fn display_status_tree(
 /// Traverse the repo and build the status tree.
 fn build_status_tree(
     args: &Args,
-    sliced_markers: HashMap<String, String>,
+    marker_map: HashMap<String, String>,
     target_directory: &str,
 ) -> Result<(StringItem, PrintConfig), NomadError> {
-    let (tree, config) = sliced_markers
+    let regex_expression = if let Some(ref pattern) = args.pattern {
+        match Regex::new(&pattern.clone()) {
+            Ok(regex) => Some(regex),
+            Err(error) => return private::Err(NomadError::RegexError(error)),
+        }
+    } else {
+        None
+    };
+
+    let (tree, config) = marker_map
         .iter()
-        .map(|(relative_path, marker)| FoundItem {
-            marker: Some(marker.to_string()),
-            path: relative_path.to_string(),
+        .map(|(relative_path, marker)| {
+            if let Some(ref regex) = regex_expression {
+                if let Some(matched) = regex.find(
+                    Path::new(&relative_path)
+                        .file_name()
+                        .unwrap_or(OsStr::new("?"))
+                        .to_str()
+                        .unwrap_or("?"),
+                ) {
+                    FoundItem {
+                        marker: Some(marker.to_string()),
+                        matched: Some((matched.start(), matched.end())),
+                        path: relative_path.clone(),
+                    }
+                } else {
+                    FoundItem {
+                        marker: Some(marker.to_string()),
+                        matched: None,
+                        path: relative_path.to_string(),
+                    }
+                }
+            } else {
+                FoundItem {
+                    marker: Some(marker.to_string()),
+                    matched: None,
+                    path: relative_path.to_string(),
+                }
+            }
         })
         .sorted_by_key(|found_item| found_item.path.to_string())
         .collect::<Vec<FoundItem>>()
