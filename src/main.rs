bf641a568b83524661cfe63beee40a40e0502a2d
Joseph Lai <jlai24142@gmail.com>
2022-02-28 20:27:33 -0500

    Adding Xterm 256 color codes into the lazy_static. Wiring up the Git blame command

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod utils;
 
 use cli::{FileTypeOptions, GitOptions, ReleaseOptions, SubCommands};
 use git::{
+    blame::bat_blame,
     commit::commit_changes,
     diff::{bat_diffs, get_repo_diffs},
     stage::stage_files,
@@ -46,6 +47,27 @@ lazy_static! {
     static ref EXTENSION_ICON_MAP: HashMap<&'static str, &'static str> = get_icons_by_extension();
     /// A HashMap containing file names with a corresponding icon.
     static ref NAME_ICON_MAP: HashMap<&'static str, &'static str> = get_icons_by_name();
+    /// Xterm 256 color codes (excludes grayscale colors).
+    ///
+    /// Corresponds with the first three color tabledefaults here:
+    ///     https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
+    static ref XTERM_COLORS: Vec<u8> = vec![
+        016, 017, 018, 019, 020, 021, 022, 023, 024, 025, 026, 027, 028, 029, 030,
+        031, 032, 033, 034, 035, 036, 037, 038, 039, 040, 041, 042, 043, 044, 045,
+        046, 047, 048, 049, 050, 051, 082, 083, 084, 085, 086, 087, 076, 077, 078,
+        079, 080, 081, 070, 071, 072, 073, 074, 075, 064, 065, 066, 067, 068, 069,
+        058, 059, 060, 061, 062, 063, 052, 053, 054, 055, 056, 057, 093, 092, 091,
+        010, 098, 088, 099, 098, 097, 096, 095, 094, 105, 104, 103, 102, 101, 100,
+        111, 110, 109, 108, 107, 106, 117, 116, 115, 114, 113, 112, 123, 122, 121,
+        120, 119, 118, 159, 158, 157, 156, 155, 154, 153, 152, 151, 150, 149, 148,
+        147, 146, 145, 144, 143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133,
+        132, 131, 130, 129, 128, 127, 126, 125, 124, 160, 161, 162, 163, 164, 165,
+        166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180,
+        181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195,
+        226, 227, 228, 229, 230, 231, 220, 221, 222, 223, 224, 225, 214, 215, 216,
+        217, 218, 219, 208, 209, 210, 211, 212, 213, 202, 203, 204, 205, 206, 207,
+        196, 197, 198, 199, 200, 201
+    ];
 }
 
 /// Run `nomad`.
@@ -192,6 +214,53 @@ fn main() -> Result<(), NomadError> {
                                     });
                                 }
                             }
+                            GitOptions::Blame(blame_options) => {
+                                match blame_options.file_number.parse::<i32>() {
+                                    Ok(file_number) => {
+                                        match indiscriminate_search(
+                                            &vec![file_number.to_string()],
+                                            Some(&repo),
+                                            SearchMode::Normal,
+                                            &target_directory,
+                                        ) {
+                                            Some(ref mut found_items) => match found_items.pop() {
+                                                Some(item) => {
+                                                    if &blame_options.lines.len() > &2 {
+                                                        println!(
+                                                            "\n{}\n",
+                                                            Colour::Red
+                                                                .bold()
+                                                                .paint("Line range only takes two values - a lower and upper bound")
+                                                        );
+                                                    } else {
+                                                        if let Err(error) = bat_blame(
+                                                            item,
+                                                            blame_options.lines.clone(),
+                                                            &repo,
+                                                            &target_directory,
+                                                        ) {
+                                                            paint_error(error);
+                                                        }
+                                                    }
+                                                }
+                                                None => println!(
+                                                    "\n{}\n",
+                                                    Colour::Red
+                                                        .bold()
+                                                        .paint("Could not find a file to blame!")
+                                                ),
+                                            },
+                                            None => println!(
+                                                "\n{}\n",
+                                                Colour::Red
+                                                    .bold()
+                                                    .paint("Could not find a file to blame!")
+                                            ),
+                                        }
+                                    }
+                                    Err(_) => paint_error(NomadError::GitBlameError),
+                                }
+                            }
                             GitOptions::Commit { message } => {
                                 if let Err(error) = commit_changes(message, &repo) {
                                     paint_error(error);
@@ -248,7 +317,7 @@ fn main() -> Result<(), NomadError> {
                             }
                         }
                     } else {
-                        paint_error(NomadError::Error(anyhow!("Cannot run Git commands here!\nThe directory does not contain a Git repository.")));
+                        paint_error(NomadError::Error(anyhow!("Cannot run Git commands here!")));
                     }
                 }
                 SubCommands::Releases(release_option) => match release_option {
