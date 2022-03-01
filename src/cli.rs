d47162da95d0cbadff87a2bcd05ffe8c8d69eb40
Joseph Lai <jlai24142@gmail.com>
2022-02-28 20:17:11 -0500

    Adding the Git blame command and a -l/--lines option

--- src/cli.rs
+++ src/cli.rs
@@ -117,6 +117,10 @@ pub enum GitOptions {
     /// This may be used after running nomad in numbered mode or with labeled directories.
     /// Enter a single or a list of numbers/labels delimited by a space.
     Add { item_labels: Vec<String> },
+    /// The `git blame` command.
+    /// This may be used after running monad in numbered mode or with labeled directories.
+    /// You can only call `git blame` on a single file.
+    Blame(BlameOptions),
     /// The `git commit` command.
     /// Optionally include a message after the command, ie. `git commit "YOUR MESSAGE HERE"`
     /// The default commit message is "Updating" if no message is included.
@@ -128,6 +132,19 @@ pub enum GitOptions {
     Restore { item_labels: Vec<String> },
     /// The `git status` command. Only display changed/unstaged files in the tree.
     Status,
+}
+
+/// This struct provides options for the `git blame` command.
+#[derive(Debug, PartialEq, StructOpt)]
+pub struct BlameOptions {
+    #[structopt(help = "Display a blame for this file")]
+    pub file_number: String,
+    #[structopt(
+        short,
+        long,
+        help = "Restrict a range of lines to display in the blame"
+    )]
+    pub lines: Vec<usize>,
 }
 
 /// This enum provides pattern matching options.
