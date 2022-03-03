b90492eeb95e22d226e7272458d1e02076effafb
Joseph Lai <jlai24142@gmail.com>
2022-03-02 20:20:33 -0500

    Adding the branch and restore modules

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,9 +1,11 @@
 //! Exposing Git functionality.
 
 pub mod blame;
+pub mod branch;
 pub mod commit;
 pub mod diff;
 pub mod markers;
+pub mod restore;
 pub mod stage;
 pub mod status;
 pub mod utils;
