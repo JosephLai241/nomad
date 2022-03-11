a85ea14c8f585da9a33db86cf709695536c08afc
Joseph Lai <jlai24142@gmail.com>
2022-03-11 02:01:57 -0500

    Adding a new layout to display errors

--- src/ui/layouts.rs
+++ src/ui/layouts.rs
@@ -23,6 +23,27 @@ pub fn get_single_line_popup_area(frame: Rect) -> Rect {
         .split(popup_layout[1])[1]
 }
 
+/// Create a centered `Rect` for error popups.
+pub fn get_error_popup_area(frame: Rect) -> Rect {
+    let error_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage(40),
+            Constraint::Percentage(20),
+            Constraint::Percentage(40),
+        ])
+        .split(frame);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(20),
+            Constraint::Percentage(60),
+            Constraint::Percentage(20),
+        ])
+        .split(error_layout[1])[1]
+}
+
 /// Create a centered popup area to display the current settings.
 pub fn get_settings_area(frame: Rect) -> Rect {
     let settings_layout = Layout::default()
