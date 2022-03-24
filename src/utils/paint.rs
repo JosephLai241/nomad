0876f85d23cf4642c839b4ffc52c5ea8f90926fe
Joseph Lai <jlai24142@gmail.com>
2022-03-23 20:20:11 -0400

    Adding a flag to display some good ol ASCII art

--- src/utils/paint.rs
+++ src/utils/paint.rs
@@ -3,8 +3,28 @@
 use crate::errors::NomadError;
 
 use ansi_term::Colour;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    /// THE BANNER 🥴.
+    static ref BANNER: &'static str = r#"
+   ________  ________  ________  ________   _______
+  ╱    ╱   ╲╱        ╲╱        ╲╱        ╲_╱       ╲
+ ╱         ╱         ╱         ╱         ╱         ╱
+╱         ╱         ╱         ╱         ╱         ╱
+╲__╱_____╱╲________╱╲__╱__╱__╱╲___╱____╱╲________╱
+"#;
+}
 
 /// Format and display a `NomadError`.
 pub fn paint_error(error: NomadError) {
     println!("\n{}\n", Colour::Red.bold().paint(error.to_string()));
+}
+
+/// Display the ASCII art for `nomad`.
+pub fn show_banner() {
+    println!(
+        "{}",
+        Colour::Fixed(172).blink().bold().paint(BANNER.to_string())
+    );
 }
