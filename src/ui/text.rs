d65695a3c7a159adf455f588a40f8d83c2dc4273
Joseph Lai <jlai24142@gmail.com>
2022-03-24 19:03:48 -0400

    Updating the help message with more info

--- src/ui/text.rs
+++ src/ui/text.rs
@@ -2,23 +2,86 @@
 
 /// The help text displayed in the help menu after pressing '?'.
 pub const HELP_TEXT: &str = r#"
- Use the directional or Vim directional keys [j, k] to scroll.
  Press <ESC> to exit this screen.
+
+ Use the directional or Vim directional keys [j, k] to scroll.
+
+ Optionally type a number or its <SHIFT> counterpart to scroll down/up `n` lines,
+ ie. '4' to scroll down 4 lines and '$' to scroll up 4 lines.
+
+ -------------------------------------------------------------------------------
 
  Table of Contents
  =================
 
- - Keybindings
- - Modes
+ * Widgets
+     + Cycling Through Widgets
+     + Normal Widget (Tree View)
+     + Breadcrumbs Widget
+     + Inspect Widget
+ * Keybindings
+     + Navigation
+     + Commands
 
+ -------------------------------------------------------------------------------
 
- Views
- =====
+ Widgets
+ =======
 
- Rootless mode has 4 views:
+ Rootless mode has 4 widgets:
 
-     * Normal/breadcrumbs
-     *
+     * Normal (tree view)
+     * Breadcrumbs
+     * Inspect (file view)
+     * Help (this widget)
+
+ The border of the active widget is colorized and other widgets are dimmed to
+ make it obvious which one you are in.
+
+ **TIP**: If you are not sure what keybindings are available for the current
+          widget, press 'K' to display a list of available keybindings.
+
+ Cycling Through Widgets
+ -----------------------
+
+ <ESC> allows you to exit/cycle through the widgets. Here is a small diagram
+ that depicts how <ESC> works.
+
+                   <ESC>            <ESC>
+     Breadcrumbs <=======> Normal <======= Inspect
+
+ You can enter the help widget at any time by pressing '?'. <ESC>ing from the
+ help widget will always take you back to the normal/tree view.
+
+              <ESC>
+     Normal <======= Help
+
+ Normal Widget (Tree View)
+ -------------------------
+
+ This is the widget you are in when you enter Rootless mode and is the leftmost
+ widget. You can control the appearance of the tree, enter into the directories
+ or files, open a file in a text editor, filter results by pattern, etc. See the
+ Keybindings section for details.
+
+ Breadcrumbs Widget
+ ------------------
+
+ This is the widget at the very top of the TUI. This enables you to enter parent
+ directories. You can traverse left or right and press <ENTER> to refresh the
+ tree with the contents of the selected directory.
+
+ Inspect Widget
+ --------------
+
+ This widget is conditionally rendered based on the current highlighted item in
+ the tree. This widget appears on the right side of the TUI if the highlighted
+ item is a file and will contain the contents of that file.
+
+ You can scroll up/down and search for patterns in the file while in this widget.
+ See the Keybindings section for details.
+
+ -------------------------------------------------------------------------------
 
  Keybindings
  ===========
@@ -31,61 +94,100 @@ pub const HELP_TEXT: &str = r#"
  Commands
  --------
 
- 0           In general, scroll to the top of the widget you are in
-
- 1 - 9       Scroll `n` lines/items down. `shift` + `n` scrolls `n` lines/items up.
-
- d           Toggle only displaying directories
-
- e           Open the selected file in a text editor (Neovim, Vim, Vi, or Nano)
-             This only applies if the selected item is a file
-
- g           Toggle Git markers
-
- h           Toggle displaying hidden items
-
- i           Toggle icons
-
- l           Toggle directory labels
-
- m           Toggle metadata
-
- n           Toggle item labels
-
- p           Toggle plain mode
-
- q           Quit rootless mode
-
- r           Refresh the tree
-
- s           Display the settings for the current tree
-
- x           Export the tree to a file. Optionally provide a filename
-
- D           Toggle disrespecting all rules specified in ignore-type files
-
- L           Toggle all labels (directories and items)
-
- R           Reset all options to its default value and refresh the current tree
-
- /           Search for a pattern. Supports regex expressions
-
- ?           Display this help message
-
- <ENTER>     If a file is selected       Enter scroll mode
-             If a directory is selected  Enter the selected directory and redraw
-                                         the tree
- <ESC>       Cycle through modes/widgets
+ Listed below are the commands you can use, which widgets support it, and a short
+ description of what it does.
 
 
- Modes
- =====
+ 0           Widgets: Normal, Inspect, Help
+                 Scroll to the top of the widget
 
- This UI has four modes:
+ 1 - 9       Widgets: Inspect, Help
+                 Scroll `n` lines down. <SHIFT> + `n` scrolls `n` lines up.
 
- * Breadcrumbs   Move focus to the breadcrumbs at the top of the TUI and
-                 select a different directory to inspect
- * Normal        This is the default mode when Rootless mode is instantiated
- * Scroll        This mode may be entered if the currently highlighted item is a file
+ d           Widget: Normal
+                 Toggle only displaying directories
 
+ e           Widgets: Normal, Inspect
+                 Open the selected file in a text editor (Neovim, Vim, Vi, or Nano)
+                 This only applies if the selected item is a file
+
+ g           Widget: Normal
+                 Toggle Git status markers
+
+ h           Widget: Normal
+                 Toggle displaying hidden items
+
+ i           Widget: Normal
+                 Toggle icons
+
+ l           Widget: Normal
+                 Toggle directory labels
+
+ m           Widget: Normal
+                 Toggle metadata
+
+ n           Widget: Normal
+                 Toggle item labels
+             Widget: Inspect
+                 If a pattern was matched, snap to the next match
+
+ p           Widget: Normal
+                 Toggle plain mode
+
+ q           Widgets: all
+                 Quit Rootless mode
+
+ r           Widget: Normal
+                 Refresh the tree
+
+ s           Widget: Normal
+                 Display the settings for the current tree
+
+ D           Widget: Normal
+                 Toggle disrespecting all rules specified in ignore-type files
+
+ K           Widget: Normal
+                 Display the keybindings available for the widget you are in
+
+ L           Widget: Normal
+                 Toggle all labels (directories and items)
+
+ N           Widget: Inspect
+                 If a pattern was matched, snap to the previous match
+
+ R           Widget: Normal
+                 Reset all options to its default value and refresh the current
+                 tree
+             Widget: Inspect
+                 Refresh the current file
+
+ /           Widgets: Normal, Inspect
+                 Search for a pattern. Supports regex expressions and available in
+                 the Normal and Inspect widgets
+
+ ?           Widgets: all
+                 Display this help message
+
+ <ENTER>     Widget: Normal
+                 If a file is selected
+                     Enter the Inspect (file view) widget
+                 If a directory is selected
+                     Enter the selected directory and refresh the tree with the
+                     contents of the new directory
+             Widget: Breadcrumbs
+                 Enter the selected directory and refresh the tree with the
+                 contents of the new directory.
+
+ <ESC>       Widget: all
+                 Cycle through modes/widgets
+
+ -------------------------------------------------------------------------------
+
+               ________  ________  ________  ________   _______
+              ╱    ╱   ╲╱        ╲╱        ╲╱        ╲_╱       ╲
+             ╱         ╱         ╱         ╱         ╱         ╱
+            ╱         ╱         ╱         ╱         ╱         ╱
+            ╲__╱_____╱╲________╱╲__╱__╱__╱╲___╱____╱╲________╱
+
+                               [ ROOTLESS ]
 "#;
